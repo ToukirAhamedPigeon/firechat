a7089ebd50f3c725247149ddd4bd5d2ec493b4df
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-05-02 20:53:29 +0600

    Tailwind 3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,14 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class", // enables class-based dark mode (use <body className="dark">)
-  plugins: [],
-};
-
-export default config;
+// tailwind.config.js
+module.exports = {
+    darkMode: ['class'],
+    content: [
+      './app/**/*.{js,ts,jsx,tsx}',
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
